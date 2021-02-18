1ff6893b9342be81e5dbb3c19932b4f8008955f2
unknown <contact@bakerkingswap.com>
2021-02-18 10:28:27 +0800

    First commit

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,114 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://cheeseswap.app/#/swap",
+      },
+      {
+        label: "Liquidity",
+        href: "https://cheeseswap.app/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Pools",
+    icon: "PoolIcon",
+    href: "/pasta",
+  },
+  {
+    label: "Lottery",
+    icon: "TicketIcon",
+    href: "/lottery",
+  },
+  {
+    label: "NFT",
+    icon: "NftIcon",
+    href: "/nft",
+  },
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      {
+        label: "Overview",
+        href: "https://info.cheeseswap.app/home",
+      },
+      {
+        label: "Tokens",
+        href: "https://info.cheeseswap.app/tokens",
+      },
+      {
+        label: "Pairs",
+        href: "https://info.cheeseswap.app/pairs",
+      },
+      {
+        label: "Accounts",
+        href: "https://info.cheeseswap.app/accounts",
+      },
+    ],
+  },
+  {
+    label: "IFO",
+    icon: "IfoIcon",
+    items: [
+      {
+        label: "Next",
+        href: "/ifo",
+      },
+      {
+        label: "History",
+        href: "/ifo/history",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/pizzafinancebsc",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "Pizza Group",
+        href: "https://t.me/PizzaFinance",
+      },
+      {
+        label: "Announcements",
+        href: "https://t.me/PizzaFinanceNews",
+      },
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/Pizza_Finance",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
